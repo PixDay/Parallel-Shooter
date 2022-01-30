f26297c80282da8282b95452b7d50c01a0bce6fb
SlavicPavlovic <mihailo.pavlovic@epitech.eu>
2022-01-30 01:01:33 +0100

    [ADD] Add the basic button functionality (scene switch TODO), triggers not handled

--- source/menu.cpp
+++ source/menu.cpp
@@ -1,19 +1,27 @@
 #include "parallel-shooter.h"
 
+// X = 220, Y = 120
 
 static void playButtonComportment(GameObject *self)
 {
-
+    sf::Vector2i mousePosition = sf::Mouse::getPosition();
+    if (mousePosition.x <= 820 && mousePosition.x >= 1040)
+        if (mousePosition.y >= 350 && mousePosition.y <= 470)
+            std::exit(100);
 }
 
 static void creditsButtonComportment(GameObject *self)
 {
-
+    sf::Vector2i mousePosition = sf::Mouse::getPosition();
+    if (mousePosition.x <= 820 && mousePosition.x >= 1040)
+        if (mousePosition.y >= 475 && mousePosition.y <= 595)
+            std::exit(200);
 }
 
 static void exitButtonComportment(GameObject *self)
 {
-
+    sf::Vector2i mousePosition = sf::Mouse::getPosition();
+    std::exit(255);
 }
 
 void InitMenu(App &app)
@@ -22,9 +30,9 @@ void InitMenu(App &app)
     DisplayableObject *creditsButton = new DisplayableObject("img/creditsButton.png", &creditsButtonComportment);
     DisplayableObject *exitButton = new DisplayableObject("img/exitButton.png", &exitButtonComportment);
 
-    sf::Vector2f playButtonPosition = {100.0f, 100.0f};
-    sf::Vector2f creditsButtonPosition = {500.0f, 500.0f};
-    sf::Vector2f exitButtonPosition= {600.0f, 600.0f};
+    sf::Vector2f playButtonPosition = {820.0f, 350.0f};
+    sf::Vector2f creditsButtonPosition = {820.0f, 475.0f};
+    sf::Vector2f exitButtonPosition= {820.0f, 600.0f};
 
     playButton->setPosition(playButtonPosition);
     creditsButton->setPosition(creditsButtonPosition);
